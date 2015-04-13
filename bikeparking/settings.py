376989a982e7ad822d76c59f47ab0fdcd04bef9e
Henry Gargan <henrygargan@gmail.com>
2015-04-13 14:00:47 -0400

    Made appfolder.

--- bikeparking/settings.py
+++ bikeparking/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'parkingmap',
+    'parking',
 )
 
 MIDDLEWARE_CLASSES = (

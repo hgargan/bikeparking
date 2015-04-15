6f66a60ff5ab1399e7f3c0c60d0ccaec0d2f7e40
Henry Gargan <henrygargan@gmail.com>
2015-04-15 14:08:04 -0400

    Added ModelForms

--- parking/views.py
+++ parking/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from parking.models import rack, review
+from parking.models import rack, review, rackForm, reviewForm
 
 # Create your views here.
 def home (request):
+    form = rackForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return render(request, "goodjob.html", {
+            
+        })
+        
     return render(request, "base.html", {
-        'racks': rack.objects.all()
+        'racks': rack.objects.all(),
+        'form': form
 
     })
+

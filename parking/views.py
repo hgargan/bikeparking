a0e7cd2406ec4885dff3760c5bce85af283d13b3
ellencurrin <ellen.c.currin@gmail.com>
2015-04-15 15:16:25 -0400

    updating branch

--- parking/views.py
+++ parking/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from parking.models import rack, review
+from django.shortcuts import render, get_object_or_404 
+from parking.models import rack as Rack
+from parking.models import review as Review
 
 # Create your views here.
 def home (request):
     return render(request, "base.html", {
-        'racks': rack.objects.all()
+        'racks': Rack.objects.all()
+    })
 
+def find (request):
+    return render(request, "find.html", {
+        'racks': Rack.objects.all()
+    })
+
+def detail (request, pk):
+    rack = get_object_or_404(Rack, id=pk)
+    reviews = rack.review_set.all()
+    return render(request, "rack.html", {
+        'rack': rack,
+        'reviews': reviews
     })

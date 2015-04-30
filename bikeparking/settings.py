853f95ce5cb8bd5002a9993c21554d92e1d5ab79
ellencurrin <ellen.c.currin@gmail.com>
2015-04-29 21:22:30 -0400

    init heroku

--- bikeparking/settings.py
+++ bikeparking/settings.py
@@ -92,10 +92,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
